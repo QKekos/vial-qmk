35a8e56e99186a0b02a858211d737d7bd143c7dc
QKekos <q.kekos.q@gmail.com>
2024-10-22 06:10:45 +0300

    added vial support

---
+++ keyboards/ktec/ergodone/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4B, 0x44, 0x0D, 0xD5, 0x5C, 0xD6, 0x31, 0x78}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
